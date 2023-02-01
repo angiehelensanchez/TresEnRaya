38805d50377b259c9e7430fa57cb0177b0dcba79
Angie Helen Rodas Sánchez <angiehelensanchez@gmail.com>
2023-02-01 16:40:13 +0000

    0.0.1

--- TresEnRaya/Program.cs
+++ TresEnRaya/Program.cs
@@ -12,15 +12,37 @@ namespace TresEnRaya
         static void Main(string[] args)
         {
             char[,] tablero = new char[3, 3];
+            Console.WriteLine(TableroVisual(tablero));
             Console.ReadLine();
-            Console.WriteLine("");
-
         }
 
+        static void InicializarTablero(ref char [,] tablero)
+    {
+        for(int contador1 = 0; contador1 < 3; contador1++)
+        {
+                for (int contador2 = 0; contador2 < 3; contador2++)
+                {
+                    tablero[contador1, contador2] = ' ';
+                }
+        }
+    }
         static string TableroVisual(char[,] tablero)
         {
             string tableroVisual = "";
-            tableroVisual = "┌─┬─┬─┐";
+            tableroVisual = "    A  B  C" + Environment.NewLine;
+            tableroVisual += "  ┌───┬───┬───┐" + Environment.NewLine;
+            tableroVisual += $"1 │ {tablero[0,0]} │ {tablero[0,1]} │ {tablero[0,2]} │" + Environment.NewLine;
+            tableroVisual += "  ├───┼───┼───┤" + Environment.NewLine;
+            tableroVisual += $"2 │ {tablero[1,0]} │ {tablero[1,1]} │ {tablero[1,2]} │" + Environment.NewLine;
+            tableroVisual += "  ├───┼───┼───┤" + Environment.NewLine;
+            tableroVisual += $"3 │ {tablero[2,0]} │ {tablero[2,1]} │ {tablero[2,2]} │" + Environment.NewLine;
+            tableroVisual += "  └───┴───┴───┘" + Environment.NewLine;
+            return tableroVisual;
+        }
+
+        static bool haGanadoAlguien(char[,] tablero)
+        {
+            
         }
     }
 }
