0743e67d26998ca57ac5334847ea9e57bedbff6e
Angie Helen Rodas Sánchez <angiehelensanchez@gmail.com>
2023-02-01 12:24:14 +0100

    Agregar archivos de proyecto.

---
+++ TresEnRaya/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TresEnRaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] tablero = new char[3, 3];
+            Console.ReadLine();
+            Console.WriteLine("");
+
+        }
+
+        static string TableroVisual(char[,] tablero)
+        {
+            string tableroVisual = "";
+            tableroVisual = "┌─┬─┬─┐";
+        }
+    }
+}
